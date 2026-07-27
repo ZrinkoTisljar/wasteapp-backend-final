a09e56bc7e4d436194f67b325640ab046eaff78d
ZrinkoTisljar <tisljarzrinko@gmail.com>
2026-07-27 21:30:53 +0200

    first commit

---
+++ src/main/java/com/example/wasteappfinal/WasteappFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.wasteappfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteappFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteappFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wasteappfinal/WasteappFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wasteappfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteappFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
